d8429e660994a99e720fe7ad5ddc975a20bbd858
Mattias Jönsson <mattulainen@gmail.com>
2021-03-30 14:59:01 +0200

    Added old commit

--- plotters.py
+++ plotters.py
@@ -4,10 +4,25 @@ from matplotlib import pyplot as plt
 
 
 def plot_vt(file):
-    with h5py.File(file, 'r') as data:
-        voltage = np.array(data['voltage'][:])
-        time = np.array(data['time'][:])
 
+    time_scale = 0.557e-11
+    voltage_scale = 5.8
+    offset = -5.8e-8
+
+    experiment_nr = 1
+
+    experiment_file = 'experimental-data/dark-count-pulse/C2k11-26-darkcountpulse-noavg-9uA{}.txt'.format(
+        str(experiment_nr).zfill(5)
+    )
+
+    # Load the data with numpy
+    experiment_data = np.genfromtxt(experiment_file, delimiter='\t', skip_header=6)[:, 3:5]
+
+    with h5py.File(file, 'r') as data:
+        voltage = np.array(data['voltage'][:]) * voltage_scale
+        time = np.array(data['time'][:]) * time_scale + offset
+
+    plt.plot(experiment_data[:, 0], experiment_data[:, 1])
     plt.plot(time, voltage)
     plt.xlabel('Time $t R / L_k$')
     plt.ylabel('Voltage $V / R I_c$')
